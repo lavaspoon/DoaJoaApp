a8ffb791ba5560505b324191b108a8572becb3db
lavaspoon <lavaspoon@naver.com>
2021-07-25 14:28:19 +0900

    Code UI Test

--- DoaJoaApp/DoaJoaApp/ViewController.swift
+++ DoaJoaApp/DoaJoaApp/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //제목
+    var titleLabel : UILabel = {
+        let label = UILabel()
+        label.text = "헬로우 월드!!!"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 50)
+        
+        return label
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+        view.addSubview(titleLabel)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 
 
