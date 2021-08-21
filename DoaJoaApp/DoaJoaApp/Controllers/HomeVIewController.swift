9039ee617ea2c9630618a294fdd6c03758b28dd8
lavaspoon <lavaspoon@naver.com>
2021-08-21 23:22:26 +0900

    1. 버튼 액션 컬렉션으로 적용 2. 버튼 클릭시, 디스패치큐로 라벨에 적용

--- DoaJoaApp/DoaJoaApp/Controllers/HomeVIewController.swift
+++ DoaJoaApp/DoaJoaApp/Controllers/HomeVIewController.swift
@@ -9,11 +9,27 @@
 import UIKit
 
 class HomeViewControllerController : UIViewController {
-    @IBOutlet weak var firstItemBtn: UIButton!
+    //
+    @IBOutlet weak var showFieldLabel: UILabel!
     @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet var itemBtns: [CirCleButton]!
+    
+    var showFieldString = ""{
+        didSet{
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else{ return }
+                self.showFieldLabel.textColor = .black
+                self.showFieldLabel.text = self.showFieldString
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         print("HomeViewController-viewDidLoad()")
+        
+        for btnItem in itemBtns {
+            btnItem.addTarget(self, action: #selector(onItemBtnsClicked(sender:)), for: .touchUpInside)
+        }
     }
     func onChangeProfileImage (){
         print("HomeViewControllerController-onChangeProfileImage() called")
@@ -22,11 +38,12 @@ class HomeViewControllerController : UIViewController {
          print("HomeViewControllerController-onBackBtnClicked() called")
         self.navigationController?.popViewController(animated: true)
     }
-    @IBAction func onfirstItemBtnClicked(_ sender: UIButton) {
-        print("HomeViewControllerController-onfirstItemBtnClicked() called")
-    }
     @IBAction func oneditProfileBtnClicked(_ sender: UIButton) {
                 print("HomeViewControllerController-oneditProfileBtnClicked() called")
     }
-    
+    @objc fileprivate func onItemBtnsClicked(sender : UIButton){
+        guard let inputString = sender.titleLabel?.text else{return}
+        showFieldString.append(inputString)
+    }
+ 
 }
