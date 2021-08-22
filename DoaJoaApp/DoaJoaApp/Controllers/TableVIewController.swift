6f2dad395f85997db2efc1f7292b4b2a2484846b
lavaspoon <lavaspoon@naver.com>
2021-08-22 23:13:04 +0900

    테이블 뷰 버튼 생성 테이블 뷰 생성

---
+++ DoaJoaApp/DoaJoaApp/Controllers/TableVIewController.swift
@@ -0,0 +1,16 @@
+//
+//  TableVIewController.swift
+//  DoaJoaApp
+//
+//  Created by lavaspoon on 2021/08/22.
+//  Copyright © 2021 lavaspoon. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("TableViewController-viewDidLoad()")
+    }
+}
