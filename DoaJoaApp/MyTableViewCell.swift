0913ec4a497f0c228b804150a3646fd915cb6952
lavaspoon <lavaspoon@naver.com>
2021-08-31 00:00:36 +0900

    테이블 뷰 셀을 불러오는 테이블 클래스 생성 15:30

---
+++ DoaJoaApp/MyTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyTableViewCell.swift
+//  DoaJoaApp
+//
+//  Created by lavaspoon on 2021/08/30.
+//  Copyright © 2021 lavaspoon. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell : UITableViewCell {
+    
+}
