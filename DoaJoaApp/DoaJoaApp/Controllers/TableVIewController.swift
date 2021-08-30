c1adae625039d8efb8de52b59134a033b2d7cd05
lavaspoon <lavaspoon@naver.com>
2021-08-30 23:56:38 +0900

    테이블뷰 뷰컨연결

--- DoaJoaApp/DoaJoaApp/Controllers/TableVIewController.swift
+++ DoaJoaApp/DoaJoaApp/Controllers/TableVIewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class TableViewController : UIViewController {
     
+    @IBOutlet weak var myTableView: UITableView!
     let contentArray = [
         "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page edit",
         "crambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchan",
