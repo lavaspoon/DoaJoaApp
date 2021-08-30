47bc647e992e1e86d97f6b2a2bd31ca3d4464f99
lavaspoon <lavaspoon@naver.com>
2021-08-30 23:53:12 +0900

    테스트 데이터 삽입

--- DoaJoaApp/DoaJoaApp/Controllers/TableVIewController.swift
+++ DoaJoaApp/DoaJoaApp/Controllers/TableVIewController.swift
@@ -9,9 +9,33 @@
 import UIKit
 
 class TableViewController : UIViewController {
+    
+    let contentArray = [
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page edit",
+        "crambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchan",
+        " over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, lo",
+        "ere are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum ",
+        "m Ipsum as their defau",
+        "ots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock"
+        
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
         print("TableViewController-viewDidLoad()")
     }
-//  MARK: ACTION
+}
+//위의 뷰 컨트롤러에서 한꺼번에 다 보면 소스가 지저분해지니 extension으로 나눈다.
+extension TableViewController : UITableViewDelegate {
+    
+}
+
+extension TableViewController : UITableViewDataSource{
+    //테이블 뷰 셀의 갯수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 }
