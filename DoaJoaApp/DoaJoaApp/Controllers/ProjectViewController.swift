d70484bbd829cf34be3bc9625e5f94d48b3816dd
lavaspoon <lavaspoon@naver.com>
2021-09-03 23:41:23 +0900

    프로젝트뷰-프로필 이미지 세팅(circle, border)

--- DoaJoaApp/DoaJoaApp/Controllers/ProjectViewController.swift
+++ DoaJoaApp/DoaJoaApp/Controllers/ProjectViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ProjectViewController : UIViewController {
-    
+    @IBOutlet weak var profileImage: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         print("ProjectViewController-viewDidLoad()")
+
+        profileImage.layer.cornerRadius = profileImage.frame.height / 2
+        profileImage.layer.borderWidth = 1
+        profileImage.layer.borderColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
     }
     @IBAction func onBackBtnClicked(_ sender: UIButton) {
         print("ProjectViewController-onBackBtnClicked() called")
