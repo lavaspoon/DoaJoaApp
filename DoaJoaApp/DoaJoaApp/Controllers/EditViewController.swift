ef0a842d1551bfa6399d5cccfa74f19c3b95aa25
lavaspoon <lavaspoon@naver.com>
2021-09-08 23:37:41 +0900

    에디트 뷰 생성 및 모달 연결

---
+++ DoaJoaApp/DoaJoaApp/Controllers/EditViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EditViewController.swift
+//  DoaJoaApp
+//
+//  Created by lavaspoon on 2021/09/08.
+//  Copyright © 2021 lavaspoon. All rights reserved.
+//
+
+import UIKit
+
+class EditViewController : UIViewController {
+    override func viewDidLoad() {
+           super.viewDidLoad()
+            print("EditViewController-viewDidLoad()")
+    }
+}
