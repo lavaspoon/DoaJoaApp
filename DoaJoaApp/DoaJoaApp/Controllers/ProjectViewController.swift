27c22df08509c0cfbfe2cd309b9700d9557570d2
lavaspoon <lavaspoon@naver.com>
2021-09-14 23:43:02 +0900

    프로젝트 뷰 - 이미지 코너 제거

--- DoaJoaApp/DoaJoaApp/Controllers/ProjectViewController.swift
+++ DoaJoaApp/DoaJoaApp/Controllers/ProjectViewController.swift
@@ -20,7 +20,7 @@ class ProjectViewController : UIViewController {
         super.viewDidLoad()
         print("ProjectViewController-viewDidLoad()")
 
-        profileImage.layer.cornerRadius = profileImage.frame.height / 2
+        //profileImage.layer.cornerRadius = profileImage.frame.height / 2
         //profileImage.layer.borderWidth = 1
         //profileImage.layer.borderColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
         
