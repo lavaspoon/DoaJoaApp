ad4c8f72c11470108a24ecbfd6ac29a7c4071947
lavaspoon <lavaspoon@naver.com>
2021-09-24 00:10:42 +0900

    프로젝트 뷰 - 에디트이미지 기능 추가

--- DoaJoaApp/DoaJoaApp/Controllers/ProjectViewController.swift
+++ DoaJoaApp/DoaJoaApp/Controllers/ProjectViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import ZImageCropper
 import CoreGraphics
-
+import YPImagePicker
 
 class ProjectViewController : UIViewController {
     @IBOutlet weak var profileImage: ZImageCropperView!
@@ -19,15 +19,20 @@ class ProjectViewController : UIViewController {
     @IBOutlet weak var sleepLabel: UILabel!
     @IBOutlet weak var drinkLabel: UILabel!
     
+    @IBOutlet weak var editImgBtn: UIButton!
+    
     var croppedImage: UIImage?
     override func viewDidLoad() {
         super.viewDidLoad()
         print("ProjectViewController-viewDidLoad()")
-
+        
+        self.editImgBtn.layer.cornerRadius = 10
         //profileImage.layer.cornerRadius = profileImage.frame.height / 2
         //profileImage.layer.borderWidth = 1
         //profileImage.layer.borderColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
         
+        //버튼클릭 액션 설정(Swift를 사용한 코드를 Objective-C 코드와 상호작용 시키기 위해 쓰는 키워드)
+        self.editImgBtn.addTarget(self, action: #selector(editProfileClicked), for: .touchUpInside)
         //라벨에 이미지 삽입
         if let text = heightLabel.text {
             //NSMutableAttributedString -> 특정 텍스트의 색상 및 폰트를 변경할 때 사용
@@ -77,6 +82,34 @@ class ProjectViewController : UIViewController {
     @IBAction func onBackBtnClicked(_ sender: UIButton) {
         print("ProjectViewController-onBackBtnClicked() called")
         croppedImage = profileImage.cropImage()
+    }
+    
+    // 프사 변경 버튼이 클릭되었을때
+    @objc fileprivate func editProfileClicked(){
+        print("MainViewController-editProfileClicked()")
+        
+        //카메라 라이브러리 세팅
+        var config = YPImagePickerConfiguration()
+        //config.screens = [.library, .photo, .video]
+        config.screens = [.library]
+        
+        let picker = YPImagePicker(configuration: config)
+        //사진이 선택되었을때
+        picker.didFinishPicking { [unowned picker] items, _ in
+            if let photo = items.singlePhoto {
+                print(photo.fromCamera) // Image source (camera or library)
+                print(photo.image) // Final image selected by the user
+                print(photo.originalImage) // original image selected by the user, unfiltered
+                print(photo.modifiedImage) // Transformed image, can be nil
+                print(photo.exifMeta) // Print exif meta data of original image.
+                //프사 이미지를 변경
+                self.profileImage.image = photo.image
+            }
+            //피커창 선택창 닫기
+            picker.dismiss(animated: true, completion: nil)
+        }
+        // 사진 선택창 보여주기
+        present(picker, animated: true, completion: nil)
     }
     
 }
